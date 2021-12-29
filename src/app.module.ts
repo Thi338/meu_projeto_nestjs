57f2678ec445a7b9a9f662175490f606748d46d5
Thiago <thiago.silca@hotmail.com>
2021-12-29 14:14:23 -0300

    Primeiro commit

---
+++ src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service'; // regras de negócio
+import { ProductsModule } from './products/products.module';
+
+@Module({
+  imports: [
+    ProductsModule,
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'store',
+      entities: [__dirname + '/**/*.entity(.ts,.js'],
+      synchronize: true 
+    })
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
+
+
---
+++ src/migrations/1640793993945-create-products-table.ts
@@ -0,0 +1,34 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class createProductsTable1640793993945 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: 'products',
+                columns: [
+                    {
+                        name: 'id',
+                        type: 'int',
+                        isGenerated: true,
+                        generationStrategy: 'increment',
+                    },
+                    {
+                        name: 'name',
+                        type: 'varchar', 
+                    },
+                    {
+                        name: 'price',
+                        type: 'double precision', 
+                    },
+                ],
+            }),
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('products');
+    
+    }
+
+}
